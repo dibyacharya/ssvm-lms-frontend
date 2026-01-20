38a0360969601696b273e0c932b0e7f03d3c7256
Rahul Gupta <replyrgupta@gmail.com>
2026-01-20 16:59:10 +0530

    added env

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://lmsbackend.bluehill-eb07d9c6.centralindia.azurecontainerapps.io/api";
+const API_URL = process.env.REACT_APP_API_URL;
 // const API_URL = "http://localhost:3000/api";
 
 const api = axios.create({
