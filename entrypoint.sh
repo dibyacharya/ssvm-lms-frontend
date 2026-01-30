f543c2d9ab2e86c310dbc18360cecb095e250cc4
dhammkiit <dhammkiit.cloud@gmail.com>
2026-01-30 09:48:08 +0000

    Added entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Fail fast if something is missing
+: "${REACT_APP_BACKEND_URL:?Missing BACKEND_URL}"
+: "${REACT_APP_AI_QUESTIONS_API_URL:?Missing CHATBOT_URL}"
+: "${REACT_APP_CHATBOT_API_URL:?Missing QGEN_URL}"
+
+echo "Injecting runtime config..."
+
+cat <<EOF > /usr/share/nginx/html/config.js
+window.RUNTIME_CONFIG = {
+  BACKEND_URL: "${REACT_APP_BACKEND_URL}",
+  CHATBOT_URL: "${REACT_APP_AI_QUESTIONS_API_URL}",
+  QGEN_URL: "${REACT_APP_CHATBOT_API_URL}"
+};
+EOF
+
+exec nginx -g "daemon off;"
